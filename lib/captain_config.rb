39a96442d368fd3042fe14577bc53ebac761ee4c
Dirk Gadsden <dirk@esherido.com>
2018-10-17 13:40:28 -0700

    Start building infrastructure to load configuration from the database

--- lib/captain_config.rb
+++ lib/captain_config.rb
@@ -3,6 +3,7 @@ require 'captain_config/version'
 module CaptainConfig
   autoload :Service, 'captain_config/service'
   autoload :ConfiguredEntry, 'captain_config/configured_entry'
+
   autoload :BaseConfig, 'captain_config/models/base_config'
   autoload :BooleanConfig, 'captain_config/models/boolean_config'
 end
--- lib/captain_config/service.rb
+++ lib/captain_config/service.rb
@@ -1,15 +1,58 @@
 class CaptainConfig::Service
   autoload :DSL, "captain_config/service/dsl"
 
+  # The statically-defined entries that this service should support.
   attr_reader :configured_entries
+
+  # The keys and values read from the database (or defaults).
+  attr_reader :configs
 
   def initialize(&block)
     @configured_entries = {}
+    @configs = nil
 
     DSL.new(self).instance_eval(&block)
   end
 
   def <<(configured_entry)
     @configured_entries[configured_entry.key] = configured_entry
+  end
+
+  def loaded?
+    !configs.nil?
+  end
+
+  # Load new values from the database (or default values from the
+  # configured entries).
+  def load
+    new_configs = {}
+
+    records = CaptainConfig::BaseConfig
+      .where(keys: configured_entries.keys)
+      .map { |record| [record.key.to_sym, record] }
+      .to_hash
+
+    configured_entries.each do |key, configured_entry|
+      record = records[key]
+
+      value = if record
+        # Check that it's the type we're expecting.
+        expected = record.klass
+        actual = configured_entry.model
+        if expected != actual
+          raise(
+            "Error loading #{key}: record class from database doesn't match " \
+            "configured class (expected: #{expected}, got: #{actual})",
+          )
+        end
+        record.value
+      else
+        configured_entry.default_value
+      end
+
+      new_configs[key] = value
+    end
+
+    @configs = new_configs
   end
 end
