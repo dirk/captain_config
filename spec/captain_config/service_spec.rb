4bbf6f9a9c4475c93fca8c7960210887ca099a22
Dirk Gadsden <dirk@esherido.com>
2018-12-27 12:23:37 -0800

    Add basic reading and writing specs for `Service`

---
+++ spec/captain_config/service_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+RSpec.describe CaptainConfig::Service do
+  subject do
+    CaptainConfig::Service.new load_after_initialize: false do
+      some_boolean :boolean, default: false
+      some_integer :integer
+      some_string :string
+    end
+  end
+
+  after do
+    CaptainConfig::BaseConfig.delete_all
+  end
+
+  describe '#[]' do
+    context 'when not loaded' do
+      it 'raises error' do
+        expect {
+          subject[:some_boolean]
+        }.to raise_error(/not loaded/i)
+      end
+    end
+
+    context 'when loaded' do
+      before do
+        subject.load
+      end
+
+      describe 'some_boolean' do
+        it 'uses default' do
+          expect(subject[:some_boolean]).to eq false
+        end
+
+        it 'returns value if set' do
+          subject[:some_boolean] = true
+          expect(subject[:some_boolean]).to eq true
+          # Check that it's still set after we reload.
+          subject.load
+          expect(subject[:some_boolean]).to eq true
+        end
+      end
+    end
+  end
+end
